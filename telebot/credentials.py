0d1eb25ddd08d3f706b8af81e72f7627361a17dd
Tiago Fernandes <tiago.andre.fernandes77@gmail.com]>
2021-02-03 12:05:38 +0000

    first commit

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1614848237:AAHFE2Cq55LYWJyOxNm5nvfPlkvWgs1FI6k"
 bot_user_name = "TestiGBot"
-URL = "https://git.heroku.com/testegaiabot.git"
+URL = "https://testegaiabot.herokuapp.com/"
