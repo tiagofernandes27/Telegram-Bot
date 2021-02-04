3a4fe8ca7cebb044410cdd0f7f4717f4bc50e4c3
Tiago Fernandes <tiago.andre.fernandes77@gmail.com]>
2021-02-04 15:05:23 +0000

    first commit

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def respond():
    if text == "/start":
        # print the welcoming message
        bot_welcome = """
-       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
+       Welcome to testegaia bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
        """
        # send the welcoming message
        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
