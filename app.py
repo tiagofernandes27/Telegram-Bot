76be4b01c6ae50fe7cdbd647b31fbbfa85d5d301
Tiago Fernandes <tiago.andre.fernandes77@gmail.com]>
2021-02-05 18:13:59 +0000

    first commit

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
+   print(request.get_json())
    # retrieve the message in JSON and then transform it to Telegram object
    update = telegram.Update.de_json(request.get_json(force=True), bot)
    print(update)
