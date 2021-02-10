6235dc07409c44a50211005e61f30dd20cc44b42
Tiago Fernandes <tiago.andre.fernandes77@gmail.com]>
2021-02-10 14:01:37 +0000

    commit

--- app.py
+++ app.py
@@ -12,12 +12,9 @@ app = Flask(__name__)
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-   print(request.get_json())
-   print(bot)
    # retrieve the message in JSON and then transform it to Telegram object
    update = telegram.Update.de_json(request.get_json(force=True), bot)
-   print(update)
-   if hasattr(update, "message"):
+   if update.edited_message == False:
        chat_id = update.message.chat.id
        msg_id = update.message.message_id
        text = update.message.text.encode('utf-8').decode()
