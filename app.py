04bd7ab07e74c7a8f25865e2a755b2269f78497d
Ricardo Silva <campos.ricardosilva@gmail.com>
2021-02-12 10:10:46 +0000

    build: deploying version 1.0.0

--- app.py
+++ app.py
@@ -17,9 +17,6 @@ def respond():
    chat_id = update.effective_message.chat.id
    msg_id = update.effective_message.message_id
    text = update.effective_message.text.encode('utf-8').decode()
-   
-   # for debugging purposes only
-   print("got text message :", text)
 
    # the first time you chat with the bot AKA the welcoming message
    if text == "/start":
@@ -43,7 +40,7 @@ def respond():
                make = "'make setup' - creates all the environment for the project, usually this command is inside API module\n'make destroy' - destroy the environment for the project, usually this command is inside API module\n'make up' - turn on cluster, this command is usually available inside API module\n'make down' - turn off cluster, this command is usually available inside API module\n'make debug' - you can debug your module inside the cluster\n'make cleanup' - clean your module if something is wrong\n'make add_dep' - add dependencies into the project without the need of overwrite requirements file\n'make rm_dep' - remove dependencies into the project without the need of overwrite requirements file\n'make logs' - shows the server activity within 10 minutes\n'make minikube_set_memory [memory]' - changes the amount of memory of your cluster"
                bot.sendMessage(chat_id=chat_id, text=make, reply_to_message_id=msg_id)
            elif text == "/projects":
-               projects = "'gaia' - G.AI.A <https://caosdata.visualstudio.com/gaia-app>\n'gaiabmg' - Gaia For BMG <https://caosdata.visualstudio.com/gaiabmg-app>\n'magiccupom' - Magic Cupom <https://caosdata.visualstudio.com/magiccupom-app>\n'scutaai' - Scuta.AI <https://caosdata.visualstudio.com/scutaai-app>"
+               projects = "'gaia' - G.AI.A\n<https://caosdata.visualstudio.com/gaia-app>\n'gaiabmg' - Gaia For BMG\n<https://caosdata.visualstudio.com/gaiabmg-app>\n'magiccupom' - Magic Cupom\n<https://caosdata.visualstudio.com/magiccupom-app>\n'scutaai' - Scuta.AI\n<https://caosdata.visualstudio.com/scutaai-app>"
                bot.sendMessage(chat_id=chat_id, text=projects, reply_to_message_id=msg_id)
            elif text == "/gitbranches":
                gitbranches = "You have to always create your branch from 'DEVELOP' branch\n\nIn https://sprints.zoho.com/ you can see your tasks\nThe name of your branch is the Card ID inside zoho\n\nOBS: Your branch have to follow some guidelines\n\n* If the card type is an issue or bug your branch has to be named as: issue/<card_id>\n* If the card type is a task or story your branch has to be named as: feature/<card_id>\n    * remember that if your branch is a story, you can name it as the parent card id\n      and make all the child tasks inside this branch"
@@ -67,5 +64,6 @@ def set_webhook():
 @app.route('/')
 def index():
    return '.'
+
 if __name__ == '__main__':
    app.run(threaded=True)
