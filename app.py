54f1ce71014107d0dc88746737202853246b3b81
Ricardo Silva <campos.ricardosilva@gmail.com>
2021-12-14 16:07:43 +0000

    build: release 1.0.0

--- app.py
+++ app.py
@@ -35,64 +35,63 @@ def respond():
         try:
             if text == "/allcommands@cagado_de_fome_bot":
                 allcommands = """
-                    Aqui tens todos os comandos que pode utilizar\n\n
-                    /gitcomment\n
-                    /wiki\n
-                    /make\n
-                    /projects\n
+                    Aqui tens todos os comandos que pode utilizar\n
+                    /gitcomment
+                    /wiki
+                    /make
+                    /projects
                     /gitbranches
                     /application_security
                 """
-                bot.sendMessage(chat_id=chat_id, text=allcommands, reply_to_message_id=msg_id)
+                bot.sendMessage(chat_id=chat_id, text=re.sub(' {2,}', allcommands), reply_to_message_id=msg_id)
             elif text == "/gitcomment@cagado_de_fome_bot":
                 comment = """
-                    * feat(shopping cart): add the amazing button\n
-                    * fix: add missing parameter to service call\n
-                      The error occurred because of <reasons>.\n
-                    * build: release version 1.0.0\n
-                    * build: update dependencies\n
-                    * refactor: implement calculation method as recursion\n
-                    * style: remove empty line\n
-                    * revert: refactor: implement calculation method as recursion\n
+                    * feat(shopping cart): add the amazing button
+                    * fix: add missing parameter to service call
+                      The error occurred because of <reasons>.
+                    * build: release version 1.0.0
+                    * build: update dependencies
+                    * refactor: implement calculation method as recursion
+                    * style: remove empty line
+                    * revert: refactor: implement calculation method as recursion
                       This reverts commit 221d3ec6ffeead67cee8c730c4a15cf8dc84897a.
                 """
-                bot.sendMessage(chat_id=chat_id, text=comment, reply_to_message_id=msg_id)
+                bot.sendMessage(chat_id=chat_id, text=re.sub(' {2,}', comment), reply_to_message_id=msg_id)
             elif text == "/wiki@cagado_de_fome_bot":
                 bot.sendMessage(chat_id=chat_id, text="https://caosdata.visualstudio.com/magiccupom-app/_wiki/wikis/magiccupom-app.wiki/2/Welcome-to-Magic-Cupom-APP")
             elif text == "/make@cagado_de_fome_bot":
                 make = """
-                    'make setup' - creates all the environment for the project, usually this command is inside API module\n
-                    'make destroy' - destroy the environment for the project, usually this command is inside API module\n
-                    'make up' - turn on cluster, this command is usually available inside API module\n
-                    'make down' - turn off cluster, this command is usually available inside API module\n
-                    'make debug' - you can debug your module inside the cluster\n
-                    'make cleanup' - clean your module if something is wrong\n
-                    'make add_dep' - add dependencies into the project without the need of overwrite requirements file\n
-                    'make rm_dep' - remove dependencies into the project without the need of overwrite requirements file\n
-                    'make logs' - shows the server activity within 10 minutes\n
+                    'make setup' - creates all the environment for the project, usually this command is inside API module
+                    'make destroy' - destroy the environment for the project, usually this command is inside API module
+                    'make up' - turn on cluster, this command is usually available inside API module
+                    'make down' - turn off cluster, this command is usually available inside API module
+                    'make debug' - you can debug your module inside the cluster
+                    'make cleanup' - clean your module if something is wrong
+                    'make add_dep' - add dependencies into the project without the need of overwrite requirements file
+                    'make rm_dep' - remove dependencies into the project without the need of overwrite requirements file
+                    'make logs' - shows the server activity within 10 minutes
                     'make minikube_set_memory [memory]' - changes the amount of memory of your cluster
                 """
-                bot.sendMessage(chat_id=chat_id, text=make,
-                                reply_to_message_id=msg_id)
+                bot.sendMessage(chat_id=chat_id, text=re.sub(' {2,}', make), reply_to_message_id=msg_id)
             elif text == "/projects@cagado_de_fome_bot":
-                projects = "'gaia' - G.AI.A\n<https://caosdata.visualstudio.com/gaia-app>\n'gaiabmg' - Gaia For BMG\n<https://caosdata.visualstudio.com/gaiabmg-app>\n'magiccupom' - Magic Cupom\n<https://caosdata.visualstudio.com/magiccupom-app>\n'scutaai' - Scuta.AI\n<https://caosdata.visualstudio.com/scutaai-app>\n'countdown-cocacola - Countdown to Holidays\n<https://caosdata.visualstudio.com/cokenft-app>'"
+                projects = "'gaia' - G.AI.A\n<https://caosdata.visualstudio.com/gaia-app>\n'gaiabmg' - Gaia For BMG\n<https://caosdata.visualstudio.com/gaiabmg-app>\n'magiccupom' - Magic Cupom\n<https://caosdata.visualstudio.com/magiccupom-app>\n'scutaai' - Scuta.AI\n<https://caosdata.visualstudio.com/scutaai-app>\n'countdown-cocacola' - Countdown to Holidays\n<https://caosdata.visualstudio.com/cokenft-app>'"
                 bot.sendMessage(chat_id=chat_id, text=projects,
                                 reply_to_message_id=msg_id)
             elif text == "/gitbranches@cagado_de_fome_bot":
                 gitbranches = """
-                    You have to always create your branch from 'DEVELOP' branch\n\n
-                    In https://sprints.zoho.com/ you can see your tasks\n
-                    The name of your branch is the Card ID inside zoho\n\n
-                    OBS: Your branch has to follow some guidelines\n\n
-                    * If the card type is an issue or bug your branch has to be named as: issue/<card_id>\n
-                    * If the card type is a task or story your branch has to be named as: feature/<card_id>\n
-                        * remember that if your branch is a story, you can name it as the parent card id\n
+                    You have to always create your branch from 'DEVELOP' branch\n
+                    In https://sprints.zoho.com/ you can see your tasks
+                    The name of your branch is the Card ID inside zoho\n
+                    OBS: Your branch has to follow some guidelines\n
+                    * If the card type is an issue or bug your branch has to be named as: issue/<card_id>
+                    * If the card type is a task or story your branch has to be named as: feature/<card_id>
+                        * remember that if your branch is a story, you can name it as the parent card id
                         and make all the child tasks inside this branch
                 """
-                bot.sendMessage(chat_id=chat_id, text=gitbranches, reply_to_message_id=msg_id)
+                bot.sendMessage(chat_id=chat_id, text=re.sub(' {2,}', gitbranches), reply_to_message_id=msg_id)
             elif text == "/application_security@cagado_de_fome_bot":
                 security = """
-                    We have this security manual to search for best pratices within application.\n
+                    We have this security manual to search for best pratices within application.
                     https://github.com/OWASP/wstg/tree/master/document
                 """
                 bot.sendMessage(chat_id=chat_id, text=security, reply_to_message_id=msg_id)
